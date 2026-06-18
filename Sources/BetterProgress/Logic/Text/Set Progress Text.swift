ea60dd281c4195e06b0539fabcae3242987068a1
David Bureš <buresdv@gmail.com>
2026-06-18 23:26:24 +0200

    + Function for setting text on the fly

---
+++ Sources/BetterProgress/Logic/Text/Set Progress Text.swift
@@ -0,0 +1,29 @@
+//
+//  Set Progress Text.swift
+//  BetterProgress
+//
+//  Created by David Bureš - P on 18.06.2026.
+//
+
+import Foundation
+import SwiftUI
+
+public enum TextTarget
+{
+    case aboveBar(String.LocalizationValue)
+    case belowBar(String.LocalizationValue)
+}
+
+public extension Progress
+{
+    func setText(to textTarget: TextTarget)
+    {
+        switch textTarget
+        {
+        case .aboveBar(let aboveBarText):
+            self.localizedDescription = String(localized: aboveBarText)
+        case .belowBar(let belowBarText):
+            self.localizedAdditionalDescription = String(localized: belowBarText)
+        }
+    }
+}
